44fef01e02f29c4a719dfca7263947b277fc8041
theakash04 <aakashkkr04@gmail.com>
2024-02-26 15:39:03 +0530

    made canvas animations

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Particle Animation
+
+This project creates a simple particle animation using HTML canvas and JavaScript. Particles are randomly generated on the screen and move with random speeds. When particles touch the edges of the canvas, they bounce off.
+
+
+
+## Usage
+
+Simply open the `index.html` file in a web browser to see the particle animation in action.
+
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
---
+++ src/script.js
@@ -0,0 +1,62 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const particalArray = [];
+
+//setting particle sdesign
+class Particles {
+  constructor() {
+    this.x = Math.random() * window.innerWidth;
+    this.y = Math.random() * window.innerHeight;
+    this.size = Math.random() * 5 + 1;
+    this.speedX = Math.random() * 3 - 1.5;
+    this.speedY = Math.random() * 3 - 1.5;
+  }
+
+  // updating particle x & y as per speed
+  update() {
+    if(this.x <= 0 || this.x >= canvas.width){
+      this.speedX *= -1;
+    }
+    if(this.y <= 0 || this.y >= canvas.height){
+      this.speedY *= -1;
+    }
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+
+  //circle draw fnc
+  draw() {
+    ctx.fillStyle = "#E2DFD2";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, 10, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+//pushing new Particle in Array
+function creation() {
+  for (let i = 0; i < 100; i++) {
+    particalArray.push(new Particles());
+  }
+}
+
+creation();
+
+//handling particle from array to draw and update its value
+function HandleParticles() {
+  for (let i = 0; i < particalArray.length; i++) {
+    particalArray[i].update();
+    particalArray[i].draw();
+  }
+}
+
+//animate function
+function Animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  HandleParticles();
+  requestAnimationFrame(Animate);
+}
+
+Animate();
